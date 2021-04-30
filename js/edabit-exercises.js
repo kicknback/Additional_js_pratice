2728128518017de2c1d6d75bb494eb95d03da4d9
kicknback <joshua.borreli@gmail.com>
2021-04-30 17:08:29 -0500

    adding factorial function

--- js/edabit-exercises.js
+++ js/edabit-exercises.js
@@ -2,6 +2,26 @@
     "use strict";
 
 
+    // const num = 45;
+    // const longFactorial = (num) => {
+    //     var bigInt = BigInt(num);
+    //     var factorial = 1n;
+    //     for (let i = 0n; i < bigInt ; i++) {
+    //         factorial *= bigInt − i;
+    //     }
+    //     return String(factorial);
+    // }
+    //console.log(longFactorial(45));
+
+    let factorialFun = num => {
+        let total = 1;
+        for (let i = 1; i <= num; i++) {
+            total *= i;
+        }
+        return total;
+    }
+    console.log(factorialFun(25));
+
 
 
 
