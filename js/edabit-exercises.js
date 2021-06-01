81aab62b083999a1acb1ca7e90d1a76298fcca33
kicknback <joshua.borreli@gmail.com>
2021-05-31 22:30:59 -0500

    Return a function from a function exercise

--- js/edabit-exercises.js
+++ js/edabit-exercises.js
@@ -93,9 +93,44 @@
     // console.log(sumCommon([1, 2, 2, 3, 2], [5, 3, 2, 2, 1], [7, 3, 2, 2, 1]));
     // console.log(sumCommon([1, 2, 2, 3], [5, 3, 2, 2], [7, 3, 2, 2]))
 
+    // ------------------------------------------------
 
 
+    // Create a function that takes a "base number" as an argument. This function should return another function which takes a new argument, and returns the sum of the "base number" and the new argument.
+    //
+    // Please check the examples below for a clearer representation of the behavior expected.
+    //
+    //     Examples
+    // Calling makePlusFunction(5) returns a new function that takes an input,
+    // and returns the result when adding 5 to it.
 
+    // const plusFive = makePlusFunction(5)
+    //
+    // plusFive(2) ➞ 7
+    //
+    // plusFive(-8) ➞ -3
+
+    // Calling makePlusFunction(10) returns a new function that takes an input,
+    // and returns the result when adding 10 to it.
+
+    // const plusTen = makePlusFunction(10)
+    //
+    // plusTen(0) ➞ 10
+    //
+    // plusTen(188) ➞ 198
+    //
+    // plusFive(plusTen(0)) ➞ 15
+
+    function makePlusFunction(baseNum) {
+        return function(num) {
+            return baseNum + num;
+        };
+    }
+
+    // let plus6 = makePlusFunction(6)
+
+    // console.log(makePlusFunction(6)(4))
+    // console.log(plus6(3));
 
 
 
